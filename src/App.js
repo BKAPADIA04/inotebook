6f390e581d26cd4e1bbce12364954fa0a4177e1b
Bhavya <bkapadia04@gmail.com>
2023-08-21 16:12:10 +0530

    React-Router Setup Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
-
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <h1>This is iNoteBook</h1>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
