3f14e1dabae51424d1efa9c6cce48082455e02d7
Bhavya <bkapadia04@gmail.com>
2023-08-22 13:36:01 +0530

    Font Awesome Used

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -4,12 +4,14 @@ export default function NotesItem(props) {
   const { note } = props;
 
   return (
-    <div className="col-md-4 my-3">
-      <div className="card">
+    <div className="col-md-3">
+      <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">{note.description} </p>
-        </div>
+          <h5 className="card-title mx-2">{note.title}</h5>
+          <p className="card-text mx-2">{note.description}</p>
+          <i className="fa-solid fa-trash mx-2"></i>
+          <i className="fa-solid fa-pen mx-2"></i>
+          </div>
       </div>
     </div>
   );
